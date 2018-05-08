143b7da2d6f9971646348d3051705a240376910b
James Abley <james.abley@gmail.com>
2018-05-08 10:09:47 +0100

    Add larger session object
    We want to measure the impact of serialising a large session object.
    This change adds a byte array filled with random bytes to the object
    being serialised.
    
    Various timing runs where conducted where the size of the byte array
    was changed, then the test was run for 30 minutes to get the JIT to
    start working, then the benchmark was taken.
    
    ```sh
    docker-compose up -d app_1
    wrk -d 30m -c 1 -t 1 http://localhost:8081/stateless-tomcat/
    wrk -d 20m -c 1 -t 1 http://localhost:8081/stateless-tomcat/
    ```
    
    The MySQL configuration change was needed to write the 10MB session
    object successfully.

--- app/src/main/java/com/eternus/tomcat/HelloController.java
+++ app/src/main/java/com/eternus/tomcat/HelloController.java
@@ -5,16 +5,20 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class HelloController {
 
     public static final String MY_USER_SESSION = "my-user-session";
 
+    @Value("session.size_bytes")
+    private int sessionSizeBytes;
+
     @RequestMapping("/")
     String index(HttpSession session) {
         if (session.getAttribute(MY_USER_SESSION) == null) {
-            session.setAttribute(MY_USER_SESSION, new CleanupObject(session.getId()));
+            session.setAttribute(MY_USER_SESSION, new CleanupObject(session.getId(), sessionSizeBytes));
         }
         return session.getId();
     }
